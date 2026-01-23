dc3f2ec76250245bc485508f57c8c2975d32e93b
Milchschlumpf <f.pinkerneil@gmail.com>
2026-01-23 23:28:15 +0100

    Initial Commit

---
+++ SearchableBug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchableBug
+//
+//  Created by Felix Pinkerneil on 23.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchableBug/SearchableBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableBugApp.swift
+//  SearchableBug
+//
+//  Created by Felix Pinkerneil on 23.01.26.
+//
+
+import SwiftUI
+
+@main
+struct SearchableBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
